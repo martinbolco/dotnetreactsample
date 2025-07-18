dbe730c92524f1f81b1b2aecdb2be1e9558fcb35
Martin Bolco <martin.bolco@sk.ibm.com>
2025-07-18 14:28:06 +0200

    removing exclusion from program.cs

--- server-api/Program.cs
+++ server-api/Program.cs
@@ -4,9 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using server_api.Data;
 using server_api.Services;
-using System.Diagnostics.CodeAnalysis;
 
-[assembly: ExcludeFromCodeCoverage]
 
 var builder = WebApplication.CreateBuilder(args);
 
